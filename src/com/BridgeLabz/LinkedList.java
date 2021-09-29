83d261c932708421991bc6668a57c1ef992ad2b2
Grid112 <sumitware1999@gmail.com>
2021-09-29 17:46:01 +0530

    Ability To Insert Data At Mentioned Position

--- src/com/BridgeLabz/LinkedList.java
+++ src/com/BridgeLabz/LinkedList.java
@@ -16,7 +16,25 @@ public class LinkedList {
         }
         return head;
     }
-    //Display Method is used to Display LinkedList
+    public Node InsertNode(Node head, int data, int insert) {
+        Node newNode = new Node(data);
+        if (head == null)
+            head = newNode;
+        else {
+            Node temp = head;
+            int position = 0;
+            while (temp.next != null && position != insert - 2) {
+                temp = temp.next;
+                position++;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+        }
+        return head;
+    }
+
+        //Display Method is used to Display LinkedList
     public void display(Node head){
         Node temp = head;
         while (temp != null){
--- src/com/BridgeLabz/LinkedListMain.java
+++ src/com/BridgeLabz/LinkedListMain.java
@@ -6,7 +6,7 @@ public class LinkedListMain {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         LinkedList linkedList = new LinkedList();
-        System.out.println("Enter The Number Of Codes");
+        System.out.println("Enter The Number Of Nodes");
         int nodes = Sc.nextInt();
         Node head = null;
         for (int i = 0; i < nodes; i++) {
@@ -14,6 +14,13 @@ public class LinkedListMain {
             int data = Sc.nextInt();
             head = linkedList.addNode(head, data);
         }
+        linkedList.display(head);
+
+        System.out.println("Give Position Where Number Is Inserted");
+        int insert = Sc.nextInt();
+        System.out.println("Number To Insert");
+        int data = Sc.nextInt();
+        linkedList.InsertNode(head, data, insert);
         linkedList.display(head);
     }
 }
